3d149b3903c1ddeb44bb35d30b6eb7210ea440dc
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2022-11-19 19:10:46 -0300

    Criando slider carousel

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,60 +1,30 @@
-var bolinhas = document.querySelector('.slides-bolinhas');
-var quantidade = document.querySelectorAll('.slides .slides-imagem');
-var atual = 0;
-var imagem = document.getElementById('slides-atual');
-var proximo = document.getElementById('slides-proximo');
-var voltar = document.getElementById('slides-voltar');
-var rolar = true;
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
 
-for(let i = 0; i < quantidade.length; i++) {
-    var div = document.createElement('div');
-    div.id = i;
-    bolinhas.appendChild(div);
-}
+controls.forEach((control) => {
+  control.addEventListener("click", (event) => {
+    isLeft = event.target.classList.contains("arrow-left");
 
-document.getElementById('0').classList.add('slides-imgAtual');
-
-var posicao = document.querySelectorAll('.slides-bolinhas div');
-
-for(let i = 0; i < posicao.length; i++) {
-    posicao[i].addEventListener('click', () => {
-        atual = posicao[i].id;
-        rolar = false;
-        slide();
-    })
-}
-
-voltar.addEventListener('click', () => {
-    atual--;
-    rolar = false;
-    slide();
-})
-
-proximo.addEventListener('click', () => {
-    atual++;
-    rolar = false;
-    slide();
-})
-
-function slide() {
-    if(atual >= quantidade.length) {
-        atual = 0;
-    } else if(atual < 0) {
-        atual = quantidade.length-1;
-    }
-
-    document.querySelector('.slides-imgAtual').classList.remove('.slides-Atual');
-    imagem.style.marginLeft = -1024*atual+'px';
-    document.getElementById(atual).classList.add('slides-imgAtual');
-}
-
-slide();
-
-setInterval(() => {
-    if(rolar) {
-        atual++;
-        slide();
+    if (isLeft) {
+      currentItem -= 1;
     } else {
-        rolar = true;
+      currentItem += 1;
     }
-}, 4000)
+
+    if (currentItem >= maxItems) {
+      currentItem = 0;
+    }
+
+    if (currentItem < 0) {
+      currentItem = maxItems - 1;
+    }
+
+    items.forEach((item) => item.classList.remove("current-item"));
+
+    items.style.display = 'none'
+
+    items[currentItem].classList.add("current-item");
+  });
+});
